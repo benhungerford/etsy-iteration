edcf552b6512e77d28ce8a1b91cc9dd019e7d9bd
benhungerford <bmhungerford@gmail.com>
2020-09-09 17:52:41 -0400

    two finished

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ items.forEach(function(list) {
 });
 const averagePrices = prices.reduce((a,b) => a + b, 0) / prices.length;
 console.log(averagePrices);
+
+// 2. Show me how to get an array of items that cost between $14.00 and $18.00 USD. Please `console.log` the answer.
+
+const cheapItems = items.filter(function(item) {
+  return item.price <= 18 && item.price >= 14 && item.currency_code == "USD"
+});
+
+console.log(cheapItems);
+
+// 3. Show me how find the item with a "GBP" currency code and print its name and price. Please `console.log` the one you find.
